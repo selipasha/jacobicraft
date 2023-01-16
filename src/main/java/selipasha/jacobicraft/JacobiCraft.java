24f73b2508bb3858b7a861d8ab3673258e6bd336
selipasha <gipwusa@gmail.com>
2023-01-16 23:21:55 +0500

    Initial commit

---
+++ src/main/java/selipasha/jacobicraft/JacobiCraft.java
@@ -0,0 +1,17 @@
+package selipasha.jacobicraft;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class JacobiCraft implements ModInitializer {
+	public static final String MOD_ID = "jacobicraft";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+		LOGGER.info("Hello Fabric world!");
+	}
+}
---
+++ src/main/java/selipasha/jacobicraft/JacobiCraftClient.java
@@ -0,0 +1,10 @@
+package selipasha.jacobicraft;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class JacobiCraftClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
